170d5b908cc92bd709548c3fe356006ccec5749e
ohAkse <segassdc1@naver.com>
2024-06-10 20:36:10 +0900

    Initial Commit

---
+++ SWIFTT/SWIFTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTTApp.swift
+//  SWIFTT
+//
+//  Created by 박유경 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
